498dbdae2231d8ee85da135a8dd2c1a8e7f1d318
Ahmed Diakite <ahmeddiakite9934@gmail.com>
2025-08-15 23:16:57 +0000

    Ajout du formulaire de commande

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { CommandeCreateForm } from './ui-components';
+import { AmplifyProvider } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyProvider>
+      <div className="App">
+        <header className="App-header">
+          <h1>Passez votre commande de Dégué</h1>
+          <CommandeCreateForm />
+        </header>
+      </div>
+    </AmplifyProvider>
   );
 }
 
